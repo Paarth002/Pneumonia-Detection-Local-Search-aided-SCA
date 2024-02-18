35e3cd17b950794bf9f145a421e052a8cee74188
Parth Bhore <81606042+Paarth002@users.noreply.github.com>
2024-02-19 04:09:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import torch.nn.functional as F
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score
 from sklearn.metrics import classification_report
-from sklearn.metrics import plot_confusion_matrix
+# from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC as SVM
